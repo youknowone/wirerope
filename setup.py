fe13ca7e8d59d26a32f0bcbd97d52506f3f4db56
Jeong YunWon <jeong@youknowone.org>
2020-07-06 04:00:30 +0900

    Fix to work with setuptools postfix releases

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 import setuptools
 
-assert tuple(map(int, setuptools.__version__.split('.'))) >= (39, 2, 0), \
+assert tuple(map(int, setuptools.__version__.split('.')[:3])) >= (39, 2, 0), \
     'Plesase upgrade setuptools by `pip install -U setuptools`'
 
 setuptools.setup()
