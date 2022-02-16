605caae14101fd139038c6a9552b177363f35a70
John Sirois <john.sirois@gmail.com>
2022-02-16 09:34:47 -0800

    Re-instate check, but use setuptools to parse.
    This way if someone wants to directly call `python setup.py`, they still
    get the warning.

--- setup.py
+++ setup.py
@@ -1,3 +1,13 @@
 import setuptools
+from pkg_resources import get_distribution
+
+try:
+    get_distribution("setuptools>=39.2.0")
+except Exception as e:
+    raise AssertionError(
+        "Please upgrade setuptools by `pip install -U setuptools`: {}".format(
+            e
+        )
+    )
 
 setuptools.setup()
